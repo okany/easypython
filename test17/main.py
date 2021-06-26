c0eea36c46b6b9691ad21b2dd614ffc3c8203811
Okan Yilmaz <oyilmaz@federatedwireless.com>
2021-06-26 15:52:02 -0400

    corrected test17

--- test17/main.py
+++ test17/main.py
@@ -1,24 +1,63 @@
 # this script finds the nth node from the end in a singly linked list
 
-class mylist(list):
+class mylist():
     def __init__(self, alist):
-        super().__init__(alist)
+        self.next = None
+        self.data = None
+        if (alist != None and alist != []):
+            # create an object with no valid atrribute if the list is None or empty
+            self.data = alist[0]
+            self.next = None
+            if(len(alist) > 1):
+                self.next = mylist(alist[1:])
+
+    def tolist(self):
+        alist = []
+        trv = self
+        while trv != None:
+            if(trv.data != None):
+                alist.append(trv.data)
+            trv = trv.next
+
+        return alist
 
     def enditem(self, num):
-        if(len(self)<num): None
-        else:
-            tmplist = []
-            for item in self[:num]:
-                tmplist.append(item)
-            for item in self[num:]:
-                tmplist.append(item)
-                tmplist.pop(0)
 
-        return(tmplist[0])
+        if(num <= 0): return None
+
+        numlist = []
+        trv  = self
+        for i in range(num):
+            if(trv is None):
+                return(None)
+            else:
+                numlist.append(trv.data)
+                trv = trv.next
+        while trv != None:
+            numlist.append(trv.data)
+            numlist.pop(0)
+            trv = trv.next
+
+        return(numlist[0])
 
 if __name__ == '__main__':
-    tlist1 = ["a","s","d","a","s","d","a","s","f","d","s","g","g","d","f","g","d","f","f","g","h","j","f","g","h","j","g","h","j"]
-
-    ml = mylist(tlist1)
+    ml = mylist(["a","s","d","a","s","d","a","s","f","d","s","g","g","d","f","g","d","f","f","g","h","j","f","g","h","j","g","h","j"])
     nth = 9
-    print("{} item from the end of list {} is {}".format(nth, ml, ml.enditem(nth)))
+    print("{}th item from the end of list {} is {}".format(nth, ml.tolist(), ml.enditem(nth)))
+
+    ml = mylist(["a","s","d","a","s","d","a","s","f","g","h","j","g","h","j"])
+    nth = 6
+    print("{}th item from the end of list {} is {}".format(nth, ml.tolist(), ml.enditem(nth)))
+
+    ml = mylist([])
+    nth = 6
+    print("{}th item from the end of list {} is {}".format(nth, ml.tolist(), ml.enditem(nth)))
+
+    ml = mylist([1])
+    nth = 1
+    print("{}st item from the end of list {} is {}".format(nth, ml.tolist(), ml.enditem(nth)))
+
+    ml = mylist(["a","s","d","a","s","d","a","s","f","g","h","j","g","h","j"])
+    nth = 0
+    print("{}th item from the end of list {} is {}".format(nth, ml.tolist(), ml.enditem(nth)))
+
